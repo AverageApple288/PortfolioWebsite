40ea7c894903c43a0993c8000998b0f1308abeaf
AverageApple288 <118760633+AverageApple288@users.noreply.github.com>
2026-06-25 18:37:36 +0100

    Populated the home page

--- app.py
+++ app.py
@@ -7,17 +7,14 @@ app = Flask(__name__)
 def home():  # put application's code here
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
 @app.route('/projects')
 def projects():
     return render_template('projects.html')
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run()
