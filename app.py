69c3027cc061da46b5b09d19f52b748b4bf95f14
AverageApple288 <118760633+AverageApple288@users.noreply.github.com>
2026-06-27 16:30:57 +0100

    Removed server config

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 app = Flask(__name__)
 
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
-app.config['SERVER_NAME'] = 'fabianbutchart.com'
 
 csp = {
     'default-src': [
